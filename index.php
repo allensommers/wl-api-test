ed7d0bc4aa51d7513f1eeffc5b10cbfe2e6e8cd5
allensommers <54182922+allensommers@users.noreply.github.com>
2019-08-23 08:49:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-
 $url = getenv("URL")."/v1/NoaaExt.json?user=".getenv("DID")."&pass=".getenv("PASSWORD")."&apiToken=".getenv("APITOKEN");
-
 $data = json_decode(file_get_contents($url));
-$extratemp = $data->davis_current_observation->temp_extra_1;
+$temp = $data->temp_f;
+$tempExtra1 = null;
+$stationName = null;
+if (property_exists($data, "davis_current_observation")) {
+  $currentObservation = $data->davis_current_observation;
+  $stationName = $currentObservation->station_name;
+  if (property_exists($currentObservation, "temp_extra_1")) {
+    $tempExtra1 = $currentObservation->temp_extra_1;
+  }
+}
 ?>
 <html>
   <head>
@@ -12,35 +19,22 @@ $extratemp = $data->davis_current_observation->temp_extra_1;
     <style>
       * {
         font-family: 'Lexend Deca', sans-serif;
-        font-size: 24px;
+        font-size: 64px;
       }
     </style>
   </head>
   <body>
     <div class="container">
       <div class="row">
-        <div class="col">Air Temperature</div>
-<?php
-       // if(property_exists($data, "davis_current_observation")) {
-//         if ($extratemp) {
-
-?>
-        <div class="col">Water Temperature</div>
-<?php
-//        }
-        
-?>
+        <div class="col"><?php echo($stationName); ?></div>
       </div>
       <div class="row">
-        <div class="col"><?php echo($data->temp_f); ?> &deg; F</div>
-<?php
-       // if(property_exists($data, "temp_extra_1")) {
-//        if ($extratemp) {
-?>
-        <div class="col"><?php echo($data->davis_current_observation->temp_extra_1); ?> &deg; F</div>
-<?php
-//        }
-?>
+        <div class="col">Air Temp</div>
+        <div class="col">Extra Temp 1</div>
+      </div>
+      <div class="row">
+        <div class="col"><?php echo($temp); ?> &deg; F</div>
+        <div class="col"><?php echo($tempExtra1); ?> &deg; F</div>
       </div>
     </div>
   </body>
